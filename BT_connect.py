b1a6312fad914b23ad46b9d63851160d4d2a41c7
Kankarollo <pawel.kankowski@gmail.com>
2020-01-12 12:40:58 +0100

    Added logging module

--- BT_connect.py
+++ BT_connect.py
@@ -2,6 +2,7 @@
 
 import subprocess
 from time import sleep
+import logging
 
 def write_to_process(proc,input):
     proc.stdin.write(input + "\n")
@@ -10,17 +11,18 @@ def write_to_process(proc,input):
 def main():
     ps = subprocess.Popen(("bluetoothctl"), stdout=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
     write_to_process(ps,f"scan on")
-    print(f"Scanning...")
-    sleep(2)
+    logging.info(f"Scanning...")
+    sleep(5)
     for mac_addr in mac_address:
         write_to_process(ps,f"pair {mac_addr}")
-        print(f"Pairing to {mac_addr}...")
-        sleep(5)
+        logging.info(f"Pairing to {mac_addr}...")
+        sleep(7)
         write_to_process(ps,f"connect {mac_addr}")
-        print(f"Connecting to {mac_addr}...")
-        sleep(5)
+        logging.info(f"Connecting to {mac_addr}...")
+        sleep(7)
     write_to_process(ps,'.')
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="[%(levelname)s]: %(message)s")
     mac_address = [""]
     main()
